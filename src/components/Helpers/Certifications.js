0b3d26aa5363d05d100f7d49878ddb6befa59879
Dennis <dennisvoutos@gmail.com>
2025-01-25 19:57:37 +0200

    added certification data

--- src/components/Helpers/Certifications.js
+++ src/components/Helpers/Certifications.js
@@ -1,29 +1,14 @@
-import { Card, Row, Col } from "antd";
 import { certificationData } from "./constants";
 export const Certifications = () => {
   return (
     <div>
       <h3 className="title">Certifications</h3>
       <hr className="line"></hr>
-      <Card style={{ margin: "20px auto", maxWidth: "600px" }}>
-        <Row gutter={[16, 16]}>
-          {/* {certificationData.map((skill, index) => (
-            <Col key={index} span={6}>
-              <div
-                style={{
-                  textAlign: "center",
-                  background: "#f0f2f5",
-                  padding: "8px",
-                  borderRadius: "5px",
-                  boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
-                }}
-              >
-                {skill}
-              </div>
-            </Col>
-          ))} */}
-        </Row>
-      </Card>
+      <ul>
+        {certificationData.map((item, i) => {
+          return <li key={i}>{item}</li>;
+        })}
+      </ul>
     </div>
   );
 };
--- src/components/Helpers/constants.js
+++ src/components/Helpers/constants.js
@@ -1,1 +1,6 @@
-export const certificationData = [{}, {}];
+const certificationData = [
+  "Boolean-UK, 3 month Full Stack software engineer course",
+  "The Examination for the Certificate of Proficiency in English (ECPE) by the University of Michigan ",
+];
+
+export { certificationData };
