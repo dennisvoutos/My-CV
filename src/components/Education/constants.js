3f9e5f620f824cf67f29a553a92c1f9de479d9ac
Dennis <dennisvoutos@gmail.com>
2025-01-25 18:59:31 +0200

    adds education details

--- src/components/Education/constants.js
+++ src/components/Education/constants.js
@@ -4,14 +4,24 @@ const educationData = [
     title: "Master of Science in Data Science and Artificial Intelligence",
     startDate: "September 2023",
     endDate: "March 2024",
-    details: ["", "", "", ""],
+    details: [
+      "Graduated top of my class with a 4.0 GPA",
+      "Gained valuable experience using Python and R for Data Science, built Machine Learning models and visualized data using R, Python and Tableu.",
+      'Worked on an individual capstone project called “Predictive Analytics for Customer Churn: Enhancing Banking Retention Strategies through Data Science". I explored how ML algorithms could be leveraged to help us predict customer churn in a bank. SVMs, Naïve bayes classifier, Random Forest classification, Linear regression and custom neural network algorithms were used, and a final model with 94% accuracy was created',
+      "I was part of a two-person group working on a capstone project with the title “Comprehensive Analysis of Brain Cancer Gene Expression Dataset for Early Detection and Classification”. Our team aimed to uncover the hidden mechanisms in our genes that promote the creation of 4 types of brain cancer. Leveraging a plethora of ML techniques, we concluded on a logistic regression model able to classify between 4 types of brain cancer with 96% accuracy.",
+    ],
   },
   {
     university: "University of Thessaly ",
     title: "Bachelor of Science in Computer Science and Biomedical Informatics",
     startDate: "October 2016",
     endDate: "November 2022",
-    details: ["", "", "", ""],
+    details: [
+      "Acquired strong foundational knowledge in computer science, including algorithms, data structures, and software development.",
+      "Specialized in biomedical informatics, focusing on the intersection of computing and healthcare.",
+      "Was chosen to participate in a 2 month internship",
+      "Was part of a 3 day seminar on Transmembrane proteins and a full day seminar in Web security",
+    ],
   },
 ];
 
