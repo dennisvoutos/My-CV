cb5cc316079846fa6ca5e027cdc583d0e74bc7f4
Dennis <dennisvoutos@gmail.com>
2025-01-25 19:41:10 +0200

    Adds projects functionality

---
+++ src/components/PersonalProjects/constants.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    name: "This CV",
+    url: "https://github.com/dennisvoutos/My-CV",
+    description:
+      "Obviously this one is first. This project is the accumulation of everything i have learned in my professional and academic career, presented in an accesible manner.",
+  },
+  {
+    name: "Full-Stack churn predictor",
+    url: "https://github.com/dennisvoutos/Predicting-customer-churn-",
+    description:
+      "This is my personal Capstone Project that i did for my Masters. It involves using a machine learning model with random forest classification to predict whether a customer will churn from a bank or not. The dataset used can be found on Kaggle here: https://www.kaggle.com/datasets/radheshyamkollipara/bank-customer-churn/data Running the react app will give you in interface, and when submitting the data an API call is done to send that data to the python server, which in turn returns the prediction. The python server is done using flask.",
+  },
+  {
+    name: "Brain cancer type predictor",
+    url: "https://github.com/dennisvoutos/Brain-cancer-prediction",
+    description:
+      "This is our group's capstone project that we did for our Masters. It involves the use of a plethora of machine learning models, all tasked with predicting the type of brain cancer a patient might develop, based on their genotype.",
+  },
+  {
+    name: "Full-Stack recipe book",
+    url: "https://github.com/dennisvoutos/IndividualProjectFrontend",
+    description:
+      "This is the front-end part of the individual project i did while doing the full-stack certification by Boolean, provided of course by ManPower group, my employer at the time. You can also find the back-end repository here: https://github.com/dennisvoutos/IndividualProjectAPI",
+  },
+];
+
+export { projects };
--- src/components/PersonalProjects/index.js
+++ src/components/PersonalProjects/index.js
@@ -1,8 +1,23 @@
+import { Card } from "antd";
+import { projects } from "./constants";
+
 export const PersonalProjects = () => {
   return (
     <div>
       <h3>Personal projects</h3>
       <hr className="line"></hr>
+      <div style={{ display: "flex" }}>
+        {projects.map((item, i) => {
+          return (
+            <Card style={{ padding: "15px", margin: "15px" }}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <h4>You can see the repository of the project here:</h4>
+              <a href={item.url}>{item.url}</a>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };
